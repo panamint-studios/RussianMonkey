1471af97a0b6d7de2216fded977666472e3c661d
Adrian Martinez <adrianjustinmartinez@gmail.com>
2020-11-08 17:36:05 -0800

    Elevator changes to make it work with player

--- Assets/Scripts/Elevator.cs
+++ Assets/Scripts/Elevator.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(BoxCollider2D))]
 public class Elevator : MonoBehaviour, IUseable
 {
     public int floor;
@@ -12,10 +11,14 @@ public class Elevator : MonoBehaviour, IUseable
     [SerializeField]
     private List<ElevatorFloor> m_floors;
     private GameObject m_compartment;
+    private SpriteRenderer m_spriteRenderer;
+    [SerializeField]
+    private Sprite m_compartmentSprite;
     private event Action<int> m_MoveUpLevel;
     private event Action<int> m_MoveDownLevel;
     private bool m_inUse = false;
     private bool m_inRange = false;
+    private GameObject m_player;
 
     private void Awake()
     {
@@ -25,40 +28,41 @@ public class Elevator : MonoBehaviour, IUseable
     private void Init()
     {
         m_compartment = new GameObject("compartment");
+        m_spriteRenderer = m_compartment.AddComponent<SpriteRenderer>();
+        m_spriteRenderer.sprite = m_compartmentSprite;
+        var boxCollider = m_compartment.AddComponent<BoxCollider2D>();
+        boxCollider.size = new Vector2(2, 1.5f);
+        boxCollider.isTrigger = true;
+        
+
         m_compartment.transform.parent = transform;
-        m_compartment.transform.localPosition = m_floors[floor].transform.localPosition;
+        var initPos = m_floors[floor].transform.localPosition;
+        m_compartment.transform.localPosition = AdjustPositionForSprite(initPos);
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             m_inRange = true;
-            
-        }
-    }
-
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Player")
-        {
-            m_inRange = false;
+            Debug.Log("In range");
+            m_player = collision.gameObject;
         }
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (m_inRange && Input.GetKeyDown(KeyCode.E))
         {
             OnUse();
         }
 
-        if (Input.GetKeyDown(KeyCode.S))
+        if (m_inUse && Input.GetKeyDown(KeyCode.S))
         {
             m_MoveDownLevel?.Invoke(floor - 1);
         }
 
-        if (Input.GetKeyDown(KeyCode.W))
+        if (m_inUse && Input.GetKeyDown(KeyCode.W))
         {
             m_MoveUpLevel?.Invoke(floor + 1);
         }
@@ -71,12 +75,28 @@ public class Elevator : MonoBehaviour, IUseable
         {
             m_MoveDownLevel += LevelInputHandler;
             m_MoveUpLevel += LevelInputHandler;
+            HidePlayer();
         }
         else
         {
             m_MoveDownLevel -= LevelInputHandler;
             m_MoveUpLevel -= LevelInputHandler;
+            ShowPlayer();
         }
+    }
+
+    private void ShowPlayer()
+    {
+        m_player.transform.parent = null;
+        m_player.transform.position = m_floors[floor].transform.position;
+        m_player.SetActive(true);
+    }
+
+    private void HidePlayer()
+    {
+        m_player.SetActive(false);
+        m_player.transform.parent = m_compartment.transform;
+        m_player.transform.localPosition = Vector3.zero;
     }
 
     private void LevelInputHandler(int newFloor)
@@ -86,7 +106,7 @@ public class Elevator : MonoBehaviour, IUseable
 
         floor = newFloor;
 
-        m_coroutine = StartCoroutine(GoToLevel_Coroutine(m_floors[floor].transform.localPosition, 1.5f));
+        m_coroutine = StartCoroutine(GoToLevel_Coroutine(AdjustPositionForSprite(m_floors[floor].transform.localPosition), 1.5f));
     }
 
     private IEnumerator GoToLevel_Coroutine(Vector3 newPos, float time)
@@ -96,10 +116,19 @@ public class Elevator : MonoBehaviour, IUseable
 
         while(elapsedTime < time)
         {
-            m_compartment.transform.localPosition = Vector3.Lerp(startPos, newPos, elapsedTime / time);
+            var goalPos = Vector3.Lerp(startPos, newPos, elapsedTime / time);
+            m_compartment.transform.localPosition = goalPos;
             elapsedTime += Time.deltaTime;
             yield return null;
         }
         m_coroutine = null;
+    }
+
+    private Vector3 AdjustPositionForSprite(Vector3 inPos)
+    {
+        var res = inPos;
+        res.x -= m_spriteRenderer.sprite.bounds.size.x / 2;
+        res.y -= m_spriteRenderer.sprite.bounds.size.y / 2;
+        return res;
     }
 }
