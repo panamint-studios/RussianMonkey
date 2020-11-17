dee67740b4cab62340c2ac7b1b0631cae72194b5
Adrian Martinez <adrianjustinmartinez@gmail.com>
2020-11-16 17:33:42 -0800

    Some necessary scene modifications

---
+++ Assets/Scripts/NotifyProperty.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotifyProperty<T>
+{
+    public event Action Updated;
+    private T m_Value;
+    public T Value
+    {
+        get
+        {
+            return m_Value;
+        }
+        set
+        {
+            if (!EqualityComparer<T>.Default.Equals(value, default(T)))
+            {
+                m_Value = value;
+                Updated?.Invoke();
+            }
+        }
+    }
+}
