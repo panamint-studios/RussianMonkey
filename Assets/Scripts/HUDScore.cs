34900818b364f8901bf012f712116fc1a479ac95
Adrian Martinez <adrianjustinmartinez@gmail.com>
2020-11-19 17:58:00 -0800

    score stuff

---
+++ Assets/Scripts/HUDScore.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDScore : MonoBehaviour
+{
+    private Text m_text;
+
+    private void Awake()
+    {
+        m_text = GetComponent<Text>();
+    }
+
+    private void Start()
+    {
+        GameState.Instance.playerState.CashUpdated += UpdateScore;
+    }
+
+    private void UpdateScore()
+    {
+        m_text.text = GameState.Instance.playerState.cash.ToString();
+    }
+}
