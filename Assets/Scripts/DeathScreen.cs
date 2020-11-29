a2972561f0b2a7dabd41adff9c88b4416eebfb8e
Adrian Martinez <adrianjustinmartinez@gmail.com>
2020-11-29 12:12:16 -0800

    Add death screen

---
+++ Assets/Scripts/DeathScreen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathScreen : MonoBehaviour
+{
+    [Header("Scenes")]
+    [SerializeField]
+    private string m_mainMenuScene;
+    [SerializeField]
+    private string m_gameScene;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(m_gameScene);
+    }
+
+    public void PlayMainMenu()
+    {
+        SceneManager.LoadScene(m_mainMenuScene);
+    }
+}
