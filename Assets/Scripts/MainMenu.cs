50a67abe398ce59eb481b289682ee06c0e8987a6
Adrian Martinez <adrianjustinmartinez@gmail.com>
2020-11-29 10:08:02 -0800

    MainMenu added

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    private string m_gameScene;
+    [SerializeField]
+    private string m_tutorialScene;
+    [SerializeField]
+    private GameObject m_modal;
+
+    private bool m_isModalActive = false;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(m_gameScene);
+    }
+
+    public void PlayTutorial()
+    {
+        SceneManager.LoadScene(m_tutorialScene);
+    }
+
+    public void ToggleModal()
+    {
+        m_isModalActive = !m_isModalActive;
+        m_modal.SetActive(m_isModalActive);
+    }
+}
